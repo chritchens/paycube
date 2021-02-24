33af27dd55037505fe1b1673b3ba5b25332974fd
chritchens <chritchens@gmail.com>
2021-02-24 09:31:13 +0100

    Start the bic module

--- src/bic.rs
+++ src/bic.rs
@@ -1,1 +1,7 @@
+//! Business Identifier Code (BIC or SWIFT BIC or SWIFT code) as defined by ISO 9362.
 
+use crate::error::Error;
+use crate::result::Result;
+
+/// [`BIC`] is an ISO 9362 BIC code.
+pub struct BIC {}
