1bcd2a27b2d4c6a86981b3be92e2e54fb32ed548
chritchens <chritchens@gmail.com>
2021-02-24 10:17:34 +0100

    Add basic bic structure

--- src/bic.rs
+++ src/bic.rs
@@ -2,6 +2,16 @@
 
 use crate::error::Error;
 use crate::result::Result;
+use crate::country_codes::CountryCode;
+
+pub struct IDCode(String);
+pub struct LocationCode(String);
+pub struct BranchCode(String);
 
 /// [`BIC`] is an ISO 9362 BIC code.
-pub struct BIC {}
+pub struct BIC {
+    pub id: IDCode,
+    pub country: CountryCode,
+    pub location: LocationCode,
+    pub branch: BranchCode,
+}
